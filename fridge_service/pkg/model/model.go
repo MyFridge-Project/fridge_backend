41db3997f2c805a8951533db2024e1cc29fb45ce
nikk <nikitakorostin@Nikitas-MacBook-Air.local>
2025-07-31 02:14:00 +0300

    model changes and go mod

--- fridge_service/pkg/model/model.go
+++ fridge_service/pkg/model/model.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Fridge struct {
-	ID        string    `json:"id" gorm:"type:uuid;primaryKey"`
+	ID        string    `json:"id" gorm:"type:char(36);primaryKey"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
